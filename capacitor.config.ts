9c3a33385401334b0324190740178f93f6a6d4d9
rilvex.Co <rmuhammad4254@gmail.co.>
2026-08-24 15:16:25 +0100

    Fix package name to com.rememes.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.rememes',
+  appId: 'com.rememes.app',
   appName: 'Rememes',
   webDir: 'dist',
   bundledWebRuntime: false
